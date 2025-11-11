b8f5a4594aed667b67858cd79e6f900c8a21ad81
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-11-11 20:02:03 +0100

    instalando paquetes necesarios en back y creando contexto

---
+++ back/ChefMind/ChefMind/Models/Database/ChefsMindContext.cs
@@ -0,0 +1,39 @@
+﻿using ChefMind.Models.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ChefMind.Models.Database
+{
+    public class ChefsMindContext : DbContext
+    {
+        private const string DATABASE_PATH = "ChefsMind.db";
+
+        private readonly Settings _settings;
+
+        DbSet<Ingredient> Ingredient {  get; set; }
+        DbSet<Recipe> Recipe { get; set; }
+        DbSet<RecipeIngredient> RecipeIngredient { get; set; }
+        DbSet<ShoppingList> ShoppingList { get; set; }
+        DbSet<User> User { get; set; }
+
+        public ChefsMindContext(IOptions<Settings> options)
+        {
+            _settings = options.Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            string serverConnection = "";
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+
+            #if DEBUG
+            options.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+            #else
+                options.UseMySql(serverConnection,ServerVersion.AutoDetect(serverConnection));
+            #endif
+        }
+
+    }
+}
---
+++ back/ChefMind/ChefMind/Services/UserService.cs
@@ -0,0 +1,6 @@
+﻿namespace ChefMind.Services
+{
+    public class UserService
+    {
+    }
+}
