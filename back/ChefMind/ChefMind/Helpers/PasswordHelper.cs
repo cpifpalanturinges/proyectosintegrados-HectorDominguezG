53b3cfa0e9a1c44421ec8c4e2471a03ac0f8e94a
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-11-24 17:40:40 +0100

    creando el helper para hashear la contraseña

---
+++ back/ChefMind/ChefMind/Helpers/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ChefMind.Helpers
+{
+    public class PasswordHelper
+    {
+        public string Hash(string password)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            byte[] inputHash = SHA256.HashData(inputBytes);
+            return Encoding.UTF8.GetString(inputHash);
+        }
+    }
+}
