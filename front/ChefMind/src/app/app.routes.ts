98551170c589d4afae64504741aefc4311d9b129
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-11-26 11:49:40 +0100

    creando el login y el componente del registro

--- front/ChefMind/src/app/app.routes.ts
+++ front/ChefMind/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { RecetasComponent } from './pages/recetas/recetas.component';
 import { DespensaComponent } from './pages/despensa/despensa.component';
 import { ForoComponent } from './pages/foro/foro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
     {path: 'recetas', component: RecetasComponent},
     {path: 'despensa', component: DespensaComponent},
-    {path: 'foro', component: ForoComponent}
+    {path: 'foro', component: ForoComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
