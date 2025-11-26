eb9de59485315a21f394446e993e789e603a9006
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-11-26 10:33:07 +0100

    actualizando el program

--- back/ChefMind/ChefMind/Program.cs
+++ back/ChefMind/ChefMind/Program.cs
@@ -1,11 +1,64 @@
+using ChefMind;
+using ChefMind.Mappers;
+using ChefMind.Models.Database;
+using ChefMind.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<Settings>(builder.Configuration.GetSection(Settings.SECTION_NAME));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAuthentication()
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+
+                    // INDICAMOS LA CLAVE
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+
+//Base de datos
+builder.Services.AddScoped<ChefsMindContext>();
+builder.Services.AddScoped<UnitOfWork>();
+
+
+//Servicios
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<UserService>();
+
+
+//Settings
+builder.Services.Configure<Settings>(builder.Configuration.GetSection("Settings"));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<Settings>>().Value);
+
+
+//Mappers
+builder.Services.AddTransient<UserMapper>();
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    });
+});
 
 var app = builder.Build();
 
