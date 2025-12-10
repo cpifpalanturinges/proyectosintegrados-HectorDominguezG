a2ff25619ae7f99a339fa31455884d43e9fcecf3
HectorDominguezG <hdomgar0208@g.educaand.es>
2025-12-10 23:29:26 +0100

    cambiando context

--- back/ChefMind/ChefMind/Models/Database/ChefsMindContext.cs
+++ back/ChefMind/ChefMind/Models/Database/ChefsMindContext.cs
@@ -5,6 +5,7 @@ namespace ChefMind.Models.Database
 {
     public class ChefsMindContext : DbContext
     {
+        private const string DATABASE_PATH = "ChefsMind.db";
         public DbSet<Ingredient> Ingredient { get; set; }
         public DbSet<Recipe> Recipe { get; set; }
         public DbSet<RecipeIngredient> RecipeIngredient { get; set; }
@@ -22,6 +23,21 @@ namespace ChefMind.Models.Database
                 .HasKey(ri => new { ri.RecipeId, ri.IngredientId });
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            /// los datos de la url
+            /// son los de electrospeed
+            string serverConnection = "Server=db34983.public.databaseasp.net; Database=db34983; Uid=db34983; Pwd=C@g3%8GaNw6+";
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+
+            #if DEBUG
+            options.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+            #else
+                options.UseMySql(serverConnection,ServerVersion.AutoDetect(serverConnection));
+            #endif
         }
     }
 }
